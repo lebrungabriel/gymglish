2771e0b7524e770e7b94ee956b23bc4f8e38a783
Gabriel Lebrun <lebrungabriel75@gmail.com>
2023-07-09 19:00:55 +0200

    first commit, project setup, tools and packages installed

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome Hubert</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
